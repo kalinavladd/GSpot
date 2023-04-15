c27c8b76fcbb5d178e500565eed1392764b00d8a
M1RRoN <mds.myst@gmail.com>
2023-04-15 11:41:02 +0400

    add developers views

--- backend/users/backend/config/urls.py
+++ backend/users/backend/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 from .yasg import urlpatterns as yasg_doc
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/developers/', include('developers.urls')),
 ]
 urlpatterns += yasg_doc
